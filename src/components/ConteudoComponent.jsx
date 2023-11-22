b1085214fa4cc2011ed45d24f8e6ffc675f129af
Álex <alex.ruan.ribeiro@gmail.com>
2023-11-22 19:23:45 -0300

    feat: add changes in card component

--- src/components/ConteudoComponent.jsx
+++ src/components/ConteudoComponent.jsx
@@ -1,22 +1,28 @@
 
 import React, { useState } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 import '../styles/ConteudoMateria.css';
 import arrow from '../assets/arrow-right.png';
 import updateIcon from '../assets/update.png';
 import deleteIcon from '../assets/delete.png';
 
-export default function ConteudoComponent({ nome, id, entidade}) {
-    
+export default function ConteudoComponent({ nome, id, entidade }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleInfo = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const arrayteste = ['teste1', 'teste2', 'teste3']
+
     const deleteData = async () => {
         try {
-            if(entidade === 'matéria'){
+            if (entidade === 'matéria') {
                 const response = await axios.delete(`http://localhost:3000/disciplina/${id}`);
             }
-            if(entidade === 'professor'){
+            if (entidade === 'professor') {
                 const response = await axios.delete(`http://localhost:3000/professor/${id}`);
             }
-           
+
 
         } catch (error) {
 
@@ -31,10 +37,12 @@ export default function ConteudoComponent({ nome, id, entidade}) {
         <div className='component-card'>
             <ul>
                 <div className='list-item-row'>
-                    <li>
+                    <li onClick={toggleInfo} className='card-tittle'>
                         <img className='arrow-right' src={arrow} alt='Seta para a direita' />
-                        {nome} 
+                        {nome}
+
                     </li>
+
                     <div className='list-item-box'>
                         <img className='update-icon' src={updateIcon} alt='Seta para a direita' />
                     </div>
@@ -43,6 +51,15 @@ export default function ConteudoComponent({ nome, id, entidade}) {
                     </div>
                 </div>
             </ul>
+            {
+                isOpen && (
+                    <ul className='infos-card'>
+                        {arrayteste.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                )
+            }
         </div>
     );
 }
